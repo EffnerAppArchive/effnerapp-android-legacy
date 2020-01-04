1e4fb560b8b8b544d3382c1f7370f5cbb755595d
SpyderScript <sebim254@gmail.com>
2020-01-04 18:42:27 +0100

    Timetable-Updates

--- app/src/main/java/de/effnerapp/effner/TimetableActivity.java
+++ app/src/main/java/de/effnerapp/effner/TimetableActivity.java
@@ -1,19 +1,30 @@
 package de.effnerapp.effner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.ViewGroup;
 
 import com.github.tlaabs.timetableview.Schedule;
 import com.github.tlaabs.timetableview.Time;
 import com.github.tlaabs.timetableview.TimetableView;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -21,15 +32,20 @@ import java.util.Objects;
 import de.effnerapp.effner.data.model.TDay;
 import de.effnerapp.effner.data.utils.DigitsParser;
 
+import static android.app.AlertDialog.THEME_DEVICE_DEFAULT_DARK;
+import static android.app.AlertDialog.THEME_HOLO_DARK;
+
 
 public class TimetableActivity extends AppCompatActivity {
+    private TimetableView timetable;
     private DigitsParser digitsParser = new DigitsParser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
-        TimetableView timetable = findViewById(R.id.timetable);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        timetable = findViewById(R.id.timetable);
         if(SplashActivity.getDataStack().getTimetable().length != 0 && !isEmpty(SplashActivity.getDataStack().getTimetable())) {
             Log.d("TA", "Generating timetable!");
 
@@ -74,6 +90,38 @@ public class TimetableActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.timetable_nav_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.navigation_timetable_settings:
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.timetable_settings_dialog, null);
+                int theme = THEME_HOLO_DARK;
+                ColorPickerDialog.Builder dialog = new ColorPickerDialog.Builder(this, THEME_DEVICE_DEFAULT_DARK)
+                        .setTitle("Wähle eine Farbe aus!")
+                        .setPreferenceName("APP_TIMETABLE_COLOR")
+                        .setPositiveButton("OK", (ColorEnvelopeListener) (envelope, fromUser) -> {
+
+                        });
+                dialog.show();
+//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                builder.setTitle("Stundenplan-Einstellungen")
+//                        .setView(view)
+//                        .setPositiveButton("OK", (dialogInterface, i) -> {})
+//                        .show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private boolean isEmpty(TDay[] timetable) {
