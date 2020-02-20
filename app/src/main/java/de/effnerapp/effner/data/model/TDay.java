64dbab388157148a7711d6e26d63416b04887c0c
Luuuuuis <F8WFQqXh4Jy4PX8>
2020-02-20 18:23:27 +0100

    Code cleanup

--- app/src/main/java/de/effnerapp/effner/data/model/TDay.java
+++
@@ -1,9 +0,0 @@
-package de.effnerapp.effner.data.model;
-
-public class TDay {
-    private int id;
-    public String one, two, three, four, five, six, seven, eight, nine, ten;
-    public int getId() {
-        return id;
-    }
-}
