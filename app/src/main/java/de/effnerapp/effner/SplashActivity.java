92a0342a64df537cc0703321b7be058946e81b10
Luuuuuis <F8WFQqXh4Jy4PX8>
2020-02-20 17:49:06 +0100

    Code cleanup

--- app/src/main/java/de/effnerapp/effner/SplashActivity.java
+++ app/src/main/java/de/effnerapp/effner/SplashActivity.java
@@ -1,7 +1,5 @@
 package de.effnerapp.effner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
@@ -13,6 +11,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.ParseException;
 
@@ -28,6 +28,14 @@ public class SplashActivity extends AppCompatActivity {
     private static DataStack dataStack;
     private static Vertretungen vertretungen = new Vertretungen();
 
+    public static DataStack getDataStack() {
+        return dataStack;
+    }
+
+    public static Vertretungen getVertretungen() {
+        return vertretungen;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,45 +45,44 @@ public class SplashActivity extends AppCompatActivity {
         new Thread(() -> {
             boolean registered;
             LoginManager loginManager = new LoginManager(this, this);
-            if(sharedPreferences.getBoolean("APP_REGISTERED", false)) {
+            if (sharedPreferences.getBoolean("APP_REGISTERED", false)) {
                 registered = loginManager.login(sharedPreferences.getString("APP_AUTH_TOKEN", ""));
             } else {
                 registered = false;
             }
             Log.d("SPLASH", "APP_REGISTERED: " + registered);
-            if(registered) {
+            if (registered) {
                 loadData();
                 Intent intent = new Intent(this, MainActivity.class);
                 Bundle bundle = getIntent().getExtras();
-                if(bundle != null) {
-                    if(bundle.getString("NOTIFICATION_CONTENT") != null) {
+                if (bundle != null) {
+                    if (bundle.getString("NOTIFICATION_CONTENT") != null) {
                         intent.putExtras(bundle);
                     }
                 }
 
                 startActivity(intent);
                 finish();
-            } else if(!loginManager.isError()) {
+            } else if (!loginManager.isError()) {
                 startActivity(new Intent(this, LoginActivity.class));
                 finish();
             }
         }).start();
     }
 
-
     private void startUp() {
         RelativeLayout layout = new RelativeLayout(this);
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.mipmap.ic_launcher_round);
-        RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(300,300);
+        RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(300, 300);
         imageParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-        imageParams.setMargins(0,775,0,0);
+        imageParams.setMargins(0, 775, 0, 0);
         layout.addView(imageView, imageParams);
         ProgressBar progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleLarge);
         progressBar.setIndeterminate(true);
         progressBar.setVisibility(View.VISIBLE);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100,100);
-        params.setMargins(0,1500,0,0);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100, 100);
+        params.setMargins(0, 1500, 0, 0);
         params.addRule(RelativeLayout.CENTER_HORIZONTAL);
         layout.addView(progressBar, params);
         setContentView(layout);
@@ -121,20 +128,12 @@ public class SplashActivity extends AppCompatActivity {
         Log.d("SPLASH", "HeaderText: " + result);
 
         //Authentication on DSB-SERVER
-        boolean login = vertretungen.login(sharedPreferences.getString("APP_DSB_LOGIN_ID",""), sharedPreferences.getString("APP_DSB_LOGIN_PASSWORD",""));
+        boolean login = vertretungen.login(sharedPreferences.getString("APP_DSB_LOGIN_ID", ""), sharedPreferences.getString("APP_DSB_LOGIN_PASSWORD", ""));
         Log.d("SPLASH", "Login: " + login);
         try {
             vertretungen.load();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    public static DataStack getDataStack() {
-        return dataStack;
-    }
-
-    public static Vertretungen getVertretungen() {
-        return vertretungen;
     }
 }
